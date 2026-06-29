198ec549ccff223f2eb6180085e025057419e0ab
baduc259 <ducthien25092k5@gmail.com>
2026-06-29 17:26:04 +0700

    upppp

---
+++ lib/screens/add_place.dart
@@ -0,0 +1,53 @@
+import 'package:favorite_place/providers/user_places.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AddPlaceScreen extends ConsumerStatefulWidget {
+  const AddPlaceScreen({super.key});
+
+  @override
+  ConsumerState<AddPlaceScreen> createState() => _AddPlaceScreenState();
+}
+
+class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
+  final _titleController = TextEditingController();
+  void _savePlace() {
+    final enteredTitle = _titleController.text;
+    if (enteredTitle.isEmpty) {
+      return;
+    }
+    ref.read(userPlaceProvider.notifier).addPlace(enteredTitle);
+    Navigator.of(context).pop();
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Add new Place')),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            TextField(
+              decoration: const InputDecoration(labelText: 'Title'),
+              controller: _titleController,
+              style: TextStyle(color: Theme.of(context).colorScheme.onSurface),
+            ),
+            const SizedBox(height: 14),
+            ElevatedButton.icon(
+              onPressed: _savePlace,
+              icon: Icon(Icons.add),
+              label: Text('Add place'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/places.dart
@@ -0,0 +1,30 @@
+import 'package:favorite_place/providers/user_places.dart';
+import 'package:favorite_place/screens/add_place.dart';
+import 'package:favorite_place/widgets/places_list.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class PlacesScreen extends ConsumerWidget {
+  const PlacesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userPlaces = ref.watch(userPlaceProvider);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your places'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => const AddPlaceScreen()),
+              );
+            },
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: PlacesList(places: userPlaces),
+    );
+  }
+}
